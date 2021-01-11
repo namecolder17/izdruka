fdb80fb038a2145762264ee083435a39d4e4bc22
namecolder17 <morty0201@mail.ru>
2021-01-11 12:49:05 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+"""Komentārus var
+rakstīt vairākās
+rindās"""
+print("Hello, world!")
+print(2 * 2)
+print(2 * 2, 2 * 3, 2 * 4, "Pitons")
+print(f"Ja saskaitīsim 5 ar 7, iegūsim {5+7}.")  #Kombinētā izdruka
+print("Sveika, " + "pasaule!")
+print("Andrejs, " * 5)
+print(0.1 + 0.2 - 0.3)
+print(365 * 24 * 60 * 60)
