0e497db3a130cd2a068d4a5b73be91ba18cac49f
namecolder17 <morty0201@mail.ru>
2021-01-11 13:21:31 +0000

    Datu ievadei

--- main.py
+++ main.py
@@ -10,3 +10,23 @@ print("Andrejs, " * 5)
 print(0.1 + 0.2 - 0.3)
 print(365 * 24 * 60 * 60)
 print()
+
+#Mainīgie
+a = 5
+print(a)
+print(a + a)
+a = a + a + a  #Ar = piešķir vērtību
+print(a)
+print(type(a))
+a = 30.1
+print(type(a))
+
+#Nodokļi
+mani_ienakumi = 367
+nodoklis = 0.23 #23%
+maniNodokli = mani_ienakumi * nodoklis
+print(maniNodokli)
+
+#Datu ievade
+x = input("Ievadi vārdu: ")
+print("Tavs ievadītais vārds ir " + x)
